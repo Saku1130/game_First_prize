2939af6d0a97154d7be13915cbdc82df0b2d80bc
saku1130 <saku0113.s@gmail.com>
2018-06-30 21:36:24 +0900

    ハイスコアの1～5位まで表示
    シーン間でのハイスコア保存、仮数値をキーを押すと表示してある状態にしてある数値気にしないで
    押すキー、Tab、左シフト、右シフト,左Alt、右Alt

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -10,29 +10,41 @@ public class Score : MonoBehaviour {
 
     //ハイスコア表示
 
-    public Text highScoreText;
-
+    public Text highScoreText;//一位
+    public Text highScoreaText;//二位
+    public Text highScorebText;//三位
+    public Text highScorecText;//四位
+    public Text highScoredText;//五位
 
     //スコア
-    private int score;
+    public static int score;
 
     //ハイスコア
-    private int highScore;
-
+    public static int highScore;//1位
+    public static int highScorea;//2位
+    public static int highScoreb;//3位
+    public static int highScorec;//4位
+    public static int highScored;//5位
 
     private string highScoreKey = "HighScore";
 
     // Use this for initialization
     void Start()
     {
-        Initialize();
+       Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //スコアがハイスコアより大きければ
-        if (highScore < score)
+        if (Input.GetKeyDown(KeyCode.Tab)) highScore = 100;
+        if (Input.GetKeyDown(KeyCode.LeftShift)) highScorea = 200;
+        if (Input.GetKeyDown(KeyCode.RightShift)) highScoreb = 300;
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) highScorec = 400;
+        if (Input.GetKeyDown(KeyCode.RightAlt)) highScored = 500;
+
+            //スコアがハイスコアより大きければ
+            if (highScore < score)
         {
             highScore = score;
         }
@@ -41,9 +53,11 @@ public class Score : MonoBehaviour {
 
         scoreText.text = score.ToString();
 
-        highScoreText.text = highScore.ToString();
-
-
+        highScoreText.text = highScore.ToString();//1位
+        highScoreaText.text = highScorea.ToString();//2位
+        highScorebText.text = highScoreb.ToString();//3位
+        highScorecText.text = highScorec.ToString();//4位
+        highScoredText.text = highScored.ToString();//5位
     }
 
     //ゲーム開始前に戻す
@@ -54,7 +68,7 @@ public class Score : MonoBehaviour {
         score = 0;
 
         //ハイスコアを取得する。保存されてなければ0を取得する。
-        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+       // highScore = PlayerPrefs.GetInt(highScoreKey, 0);
 
     }
 
@@ -69,6 +83,10 @@ public class Score : MonoBehaviour {
         //ゲーム開始の状態を戻す
         Initialize();
     }
+
+
+    //ランキング
+
 
 }
 
